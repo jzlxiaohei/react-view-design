1f5243f1aae7321060493623ec6732f1bd6e9a19
zilong <Leonard.jiang@liulishuo.com>
2017-05-30 17:24:48 +0800

    add text to registerTable

--- src/widget/registerWidget.js
+++ src/widget/registerWidget.js
@@ -1,6 +1,8 @@
 import registerTable from 'globals/registerTable';
 import { ModelContainer, CompContainer } from './Container';
 import { ModelPicture, CompPicture } from './Picture';
+import { ModelText, CompText } from './Text';
+
 
 function register() {
   if (module.hot) {
@@ -8,6 +10,7 @@ function register() {
   }
   registerTable.register('container', ModelContainer, CompContainer, { title: '容器' });
   registerTable.register('picture', ModelPicture, CompPicture, { title: '图片' });
+  registerTable.register('text', ModelText, CompText, { title: '文本' });
 }
 
 register();
