1212a86772afe92e5589688910d552f4f47d6000
zilong <Leonard.jiang@liulishuo.com>
2017-06-11 17:08:23 +0800

    add click to select current model

--- server/require-hook.js
+++ server/require-hook.js
@@ -4,20 +4,20 @@ const path = require('path');
 const sass = require('node-sass');
 const processStyleText = require('./utils/processStyleText');
 
-const originJsExtension = require.extensions['.js'];
+// const originJsExtension = require.extensions['.js'];
 
 // m means module
-require.extensions['.js'] = (m, filename) => {
-  const devScriptFilenameReg = /[a-zA-Z0-9_-]+\.tpl-script\.js$/;
-  if (devScriptFilenameReg.test(filename)) {
-    // const fsContent = fs.readFileSync(filename).toString();
-    if (process.env.NODE_ENV !== 'production') {
-      delete require.cache[filename];
-    }
-    return m._compile('module.exports = ' + JSON.stringify(filename), filename);
-  }
-  return originJsExtension(m, filename);
-};
+// require.extensions['.js'] = (m, filename) => {
+//   const devScriptFilenameReg = /[a-zA-Z0-9_-]+\.tpl-script\.js$/;
+//   if (devScriptFilenameReg.test(filename)) {
+//     // const fsContent = fs.readFileSync(filename).toString();
+//     if (process.env.NODE_ENV !== 'production') {
+//       delete require.cache[filename];
+//     }
+//     return m._compile('module.exports = ' + JSON.stringify(filename), filename);
+//   }
+//   return originJsExtension(m, filename);
+// };
 
 function hook(compile, extension) {
   require.extensions[extension] = (m, filename) => {
